0c57959959d5e7eb9b3007a24ccb5a86c1f4fadf
kramoris <kramoris@proton.me>
2026-03-24 23:32:29 +0100

    add player collisions with asteroids

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+import sys
+
 import pygame
 
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
-from logger import log_state
+from logger import log_event, log_state
 from player import Player
 
 
@@ -18,7 +20,7 @@ def main():
     Player.containers = (updatable, drawable)
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = (updatable,)
-    
+
     asteroidfield = AsteroidField()
 
     pygame.init()
@@ -38,7 +40,14 @@ def main():
 
         for sprite in drawable:
             sprite.draw(screen)
+
         updatable.update(dt)
+
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                log_event("player_hit")
+                print("Game over!")
+                sys.exit()
 
         pygame.display.flip()
         dt = clock.tick(60) / 1000
